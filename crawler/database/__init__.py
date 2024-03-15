ad5a76b5501688e7fe71f505369956fafb3a5461
Baluf <162377261+Baluf@users.noreply.github.com>
2024-03-15 23:09:19 +0200

    Update __init__.py

--- crawler/database/__init__.py
+++ crawler/database/__init__.py
@@ -4,12 +4,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_AT_BAY_URL = os.environ.get('DATABASE_URL')
+DATABASE_URL = os.environ.get('DATABASE_URL')
 
-if not DATABASE_AT_BAY_URL:
+if not DATABASE_URL:
     raise ValueError("DATABASE_URL environment variable is not set")
 
-engine = create_engine(DATABASE_AT_BAY_URL)
+engine = create_engine(DATABASE_URL)
 
 session_factory = sessionmaker(bind=engine)
 Session = scoped_session(session_factory)
